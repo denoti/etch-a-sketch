c09434b515e1e562aa7688b4ebe1ee4b4c76d3fb
denoti <otidennis98@gmail.com>
2023-10-18 21:42:28 +0300

    Added change color functionality as required

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
+let gridItems;
 
 let noOfGridItems = 16;
 
@@ -12,6 +13,7 @@ function createGridItems(numOfGridItems) {
   }
   container.style.gridTemplateColumns = `repeat(${noOfGridItems},1fr`;
   container.style.gridTemplateRows = `repeat(${noOfGridItems}, 1fr`;
+  gridItems = Array.from(document.querySelectorAll('.grid-item'));
 }
 
 button.onclick = () => {
@@ -19,7 +21,29 @@ button.onclick = () => {
   if (!num) return;
   if (num > 100) num = 100;
   noOfGridItems = num;
-  createGridItems(noOfGridItems);
+  createGridItems( noOfGridItems );
+  gridColorChange()
 };
-
 createGridItems(noOfGridItems);
+function changeColor() {
+  let r = Math.floor(Math.random() * 256) + 0;
+  let g = Math.floor(Math.random() * 256) + 0;
+  let b = Math.floor(Math.random() * 256) + 0;
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+function gridColorChange () {
+  gridItems.forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+      item.style.backgroundColor = changeColor();
+    });
+    item.addEventListener('mouseleave', () => {
+      item.style.backgroundColor = changeColor();
+    });
+  });
+}
+
+gridColorChange()
+
+
